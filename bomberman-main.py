b0f098b5cdcebeb6c3e19bd4151e9d0abb987db9
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-02-20 23:02:54 +0100

    Tidying up

--- bomberman-main.py
+++ bomberman-main.py
@@ -17,32 +17,6 @@ import pygame
 import keyboard
 import numpy as np
 
-pygame.init()
-
-display_width = 640
-display_height = 480
-
-gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('A bit Racey')
-
-black = (0, 0, 0)
-white = (255, 255, 255)
-
-clock = pygame.time.Clock()
-crashed = False
-# carImg = pygame.image.load('racecar.png')
-carImg = pygame.image.load('./sdkskin/Sprites.bmp')
-# Sprites.bmp
-print('')
-crateImg = pygame.image.load('./sdkskin/Sprites.bmp')
-
-
-def car(x, y):
-    gameDisplay.blit(carImg, (x, y))
-
-
-def crate(x,y):
-    gameDisplay.blit(crateImg, (x, y))
 
 def currentMap():
     map = np.ones((15,20))
@@ -60,6 +34,29 @@ def tileGen():
     for line in range(15):
         for row in range(20):
             yield(row,line)
+
+pygame.init()
+
+display_width = 640
+display_height = 480
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('A bit Racey')
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+clock = pygame.time.Clock()
+crashed = False
+# carImg = pygame.image.load('racecar.png')
+carImg = pygame.image.load('./sdkskin/Sprites.bmp')
+crateImg = pygame.image.load('./sdkskin/Sprites.bmp')
+
+def car(x, y):
+    gameDisplay.blit(carImg, (x, y))
+
+def crate(x,y):
+    gameDisplay.blit(crateImg, (x, y))
 
 x = (display_width * 0.45)
 y = (display_height * 0.8)
