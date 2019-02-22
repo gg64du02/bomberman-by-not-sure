10cfaa7f725f7d1ad158d5b309d26b468926f175
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-02-23 00:18:56 +0100

    Improving the d key support

--- bomberman-main.py
+++ bomberman-main.py
@@ -131,8 +131,10 @@ def ColisionCheckAndMovement():
             if(player[0][1]+32>480):
                 player[0][1] =480-32
             # if(TheMap[int((player[0][1] +24)/32),int((player[0][1] +24)/32)]==0):
-            if(TheMap[int((player[0][1]+24)/32),int((player[0][0]+0)/32)]==0):
+            if((TheMap[int((player[0][1]+24)/32),int((player[0][0]+0)/32)]==0)or(TheMap[int((player[0][1]+24)/32),int((player[0][0]+24)/32)]==0)):
                 player[0][1]-= step
+            # if(TheMap[int((player[0][1]+24)/32),int((player[0][0]+24)/32)]==0):
+            #     player[0][1]-= step
         # e
         if(control[0][3]==1):
             if (TheMap[int(player[0][1] / 32), int(player[0][0] / 32)] == 1):
