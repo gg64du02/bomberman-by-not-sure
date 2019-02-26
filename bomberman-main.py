7ef6f0b10b1c3f740c4406d4b997b3c31b16b3fd
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-02-26 23:21:28 +0100

    cleaning a subsurface

--- bomberman-main.py
+++ bomberman-main.py
@@ -52,8 +52,6 @@ playerRedDeadImg = pygame.image.load('./sdkskin/redPDead.png')
 bombImg = pygame.image.load('./sdkskin/Bomb.bmp')
 blastingAirImg = pygame.image.load('./sdkskin/BlastingAir.png')
 tileImg = pygame.image.load('./sdkskin/Tile.bmp')
-# tileImg2 = tileImg.subsurface(pygame.Rect(64,64,96,96))
-tileImg2 = tileImg.subsurface(pygame.Rect(0,0,32,32))
 
 
 TheMap = currentMap()
@@ -74,7 +72,6 @@ def block(x,y):
 
 def crate(x,y):
     gameDisplay.blit(crateImg, (x, y))
-    # gameDisplay.blit(tileImg2, (x, y))
 
 def airBlast(x,y):
     gameDisplay.blit(blastingAirImg, (x, y))
