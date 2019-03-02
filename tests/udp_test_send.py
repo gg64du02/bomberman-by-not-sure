a463378305e4ec824a347235461c379421806152
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-03-02 15:08:23 +0100

    PoC for TCP/UDP

---
+++ tests/udp_test_send.py
@@ -0,0 +1,15 @@
+# Toggle line numbers
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = "Hello, World!"
+MESSAGE_bytes = MESSAGE.encode()
+
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+print("message:", MESSAGE)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE_bytes, (UDP_IP, UDP_PORT))
