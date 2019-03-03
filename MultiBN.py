48a5e9c3ca855b8bcf77b7fbdf73dc2dcff96717
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-03-03 21:13:22 +0100

    Working on networking constants

--- MultiBN.py
+++ MultiBN.py
@@ -1,5 +1,31 @@
 # MultiBN constants
-MBN_SESSION_TCP_INIT = 0
+
+MBN_TCP_CLIENT_JOIN_REQUIRED = 0
+# Used by the client to require to join a game
+# Used by the server to check if a new client can join (any room left ?)
+MBN_TCP_SERVER_JOIN_REFUSED = -1
+# Used by the server to answer to MBN_TCP_CLIENT_JOIN_REQUIRED to refuse a join (Client drop the connexion)
+MBN_TCP_SERVER_JOIN_ACCEPTED = 1
+# Used by the server to answer to MBN_TCP_CLIENT_JOIN_REQUIRED to accept a join
+
+
+MBN_SESSION_TCP_SERVER_NUMBER_OF_AVAILABLE_PLAYERS_SLOTS = 2
+# Used by the server to let know the clients how many local players there are left (after MBN_TCP_SERVER_JOIN_ACCEPTED)
+
+MBN_SESSION_TCP_CLIENT_NUMBER_OF_LOCAL_PLAYERS = 3
+# Used by the client to answer to MBN_SESSION_TCP_SERVER_NUMBER_OF_AVAILABLE_PLAYERS_SLOTS to let know the server how many
+# local players there are
+
+MBN_SESSION_TCP_CLIENT_PLAYERS_ARE_READY = 4
+# Used by the client to (after MBN_TCP_SERVER_JOIN_ACCEPTED) to let know the players' client are ready
+
+MBN_SESSION_TCP_SERVER_BEFORE_COUNTDOWN_FIRST_ROUND = 5
+# Used by the server to if the UDP packets can ping at least once per client
+
+MBN_SESSION_TCP_SERVER_COUNTDOWN_FIRST_ROUND = 6
+# Used by the server to tell the client to prepare processing the incoming UDP packets about the game info.
+
+
 MBN_SESSION_TCP_QUIT = 1
 MBN_SESSION_TCP_CLIENT_NUMBER_OF_LOCAL_PLAYERS = 2
 MBN_SESSION_TCP_NEW_CLIENT = 3
