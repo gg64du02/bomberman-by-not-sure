05c0f68e3803784f42e07ff70ccd92eef567f019
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-03-24 14:21:53 +0100

    Changing the update URL

--- auto-update-bomberman-main.py
+++ auto-update-bomberman-main.py
@@ -2,7 +2,7 @@ import wget
 import os
 import time
 
-url = 'https://raw.githubusercontent.com/gg64du02/bomberman-by-not-sure/dev-cleaning-logs/bomberman-main.py'
+url = 'https://raw.githubusercontent.com/gg64du02/bomberman-by-not-sure/blob/master/bomberman-main.py'
 
 # while(True):
 #     time.sleep(10)
