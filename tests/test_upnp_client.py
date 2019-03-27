f1d7bb1821d95243ea04d2fbf1d7a938892b090a
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-03-27 23:15:13 +0100

    Adding more code the example

--- tests/test_upnp_client.py
+++ tests/test_upnp_client.py
@@ -12,3 +12,16 @@ print(d.WANIPConn1.GetStatusInfo())
 
 print(d.WANIPConn1.GetExternalIPAddress())
 
+print(d.WANIPConn1.AddPortMapping.argsdef_in)
+
+# print(d.WANIPConn1.AddPortMapping(
+#     NewRemoteHost='0.0.0.0',
+#     NewExternalPort=12345,
+#     NewProtocol='TCP',
+#     NewInternalPort=12345,
+#     NewInternalClient='192.168.1.10',
+#     NewEnabled='1',
+#     NewPortMappingDescription='Testing',
+#     NewLeaseDuration=10000)
+# )
+
