76ce44f803aef2bd3557a61b86c0874b4d579f0d
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-03-30 00:48:08 +0100

    Adding the upnp port removal when finnished

--- tests/test_upnp_client.py
+++ tests/test_upnp_client.py
@@ -8,18 +8,21 @@ print(devices)
 
 d = devices[0]
 
-# print(d.WANIPConn1.GetStatusInfo())
-#
-# print(d.WANIPConn1.GetExternalIPAddress())
-#
-# print(d.WANIPConn1.AddPortMapping.argsdef_in)
-#
-# print(d.WANIPConn1)
+# use the debug in pyCharm here to see what is available
+print(d.WANIPConn1.GetStatusInfo())
+
+print(d.WANIPConn1.GetExternalIPAddress())
+
+print(d.WANIPConn1.AddPortMapping.argsdef_in)
+
+print(d.WANIPConn1.GetExternalIPAddress())
+
+print(d.WANIPConn1)
 
 print(d.WANIPConn1.AddPortMapping(
     # NewRemoteHost='192.168.1.99',
     # pro tip: never thrust the error output coming from the upnp device,
-    # it could name a problme that is not the actual problem
+    # it could name a problem that is not the actual problem
     NewRemoteHost='',
     NewExternalPort=int(5010),
     NewProtocol='TCP',
@@ -30,26 +33,12 @@ print(d.WANIPConn1.AddPortMapping(
     NewLeaseDuration=10000)
 )
 
+print(d.WANIPConn1.DeletePortMapping(
+    # NewRemoteHost='192.168.1.99',
+    # pro tip: never thrust the error output coming from the upnp device,
+    # it could name a problem that is not the actual problem
+    NewRemoteHost='',
+    NewExternalPort=int(5010),
+    NewProtocol='TCP')
+)
 
-# print(d.WANIPConn1.AddPortMapping(
-#     NewRemoteHost='0.0.0.0',
-#     NewExternalPort=12345,
-#     NewProtocol='TCP',
-#     NewInternalPort=12345,
-#     NewInternalClient='192.168.1.10',
-#     NewEnabled='1',
-#     NewPortMappingDescription='Testing',
-#     NewLeaseDuration=int(10000))
-# )
-
-
-# print(d.WANIPConn1.AddPortMapping(
-#     NewRemoteHost='0.0.0.0',
-#     NewExternalPort=12345,
-#     NewProtocol='TCP',
-#     NewInternalPort=12345,
-#     NewInternalClient='192.168.1.10',
-#     NewEnabled='1',
-#     NewPortMappingDescription='Testing',
-#     NewLeaseDuration=10000)
-# )
