95f2116ffc38bf571033e2dd0ab2379201787f4d
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-03-30 00:21:51 +0100

    Adding note and changing to any incoming connection

--- tests/test_upnp_client.py
+++ tests/test_upnp_client.py
@@ -17,7 +17,10 @@ d = devices[0]
 # print(d.WANIPConn1)
 
 print(d.WANIPConn1.AddPortMapping(
-    NewRemoteHost='192.168.1.99',
+    # NewRemoteHost='192.168.1.99',
+    # pro tip: never thrust the error output coming from the upnp device,
+    # it could name a problme that is not the actual problem
+    NewRemoteHost='',
     NewExternalPort=int(5010),
     NewProtocol='TCP',
     NewInternalPort=int(5010),
