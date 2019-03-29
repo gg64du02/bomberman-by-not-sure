2d10d529ebb005ffd04848baf974bad1f18f2061
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-03-29 22:45:38 +0100

    Proposing a fix for the script

--- tests/test_upnp_client.py
+++ tests/test_upnp_client.py
@@ -8,11 +8,37 @@ print(devices)
 
 d = devices[0]
 
-print(d.WANIPConn1.GetStatusInfo())
-
-print(d.WANIPConn1.GetExternalIPAddress())
-
+# print(d.WANIPConn1.GetStatusInfo())
+#
+# print(d.WANIPConn1.GetExternalIPAddress())
+#
 print(d.WANIPConn1.AddPortMapping.argsdef_in)
+
+print(d.WANIPConn1)
+
+print(d.WANIPConn1.AddPortMapping(
+    NewRemoteHost='0.0.0.0',
+    NewExternalPort=0xff00,
+    NewProtocol='TCP',
+    NewInternalPort=0xff00,
+    NewInternalClient='192.168.1.10',
+    NewEnabled='1',
+    NewPortMappingDescription='Testing',
+    NewLeaseDuration=0xff00ff00)
+)
+
+
+# print(d.WANIPConn1.AddPortMapping(
+#     NewRemoteHost='0.0.0.0',
+#     NewExternalPort=12345,
+#     NewProtocol='TCP',
+#     NewInternalPort=12345,
+#     NewInternalClient='192.168.1.10',
+#     NewEnabled='1',
+#     NewPortMappingDescription='Testing',
+#     NewLeaseDuration=int(10000))
+# )
+
 
 # print(d.WANIPConn1.AddPortMapping(
 #     NewRemoteHost='0.0.0.0',
@@ -24,4 +50,3 @@ print(d.WANIPConn1.AddPortMapping.argsdef_in)
 #     NewPortMappingDescription='Testing',
 #     NewLeaseDuration=10000)
 # )
-
