cb51a26f57bcabf49ad5ebd77d58bf38ba46627a
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-04-05 00:20:16 +0200

    Updating the test script

--- tests/test_upnp_client.py
+++ tests/test_upnp_client.py
@@ -19,19 +19,19 @@ print(d.WANIPConn1.GetExternalIPAddress())
 
 print(d.WANIPConn1)
 
-print(d.WANIPConn1.AddPortMapping(
-    # NewRemoteHost='192.168.1.99',
-    # pro tip: never thrust the error output coming from the upnp device,
-    # it could name a problem that is not the actual problem
-    NewRemoteHost='',
-    NewExternalPort=int(5010),
-    NewProtocol='TCP',
-    NewInternalPort=int(5010),
-    NewInternalClient='192.168.1.99',
-    NewEnabled='true',
-    NewPortMappingDescription='Testing',
-    NewLeaseDuration=10000)
-)
+# print(d.WANIPConn1.AddPortMapping(
+#     # NewRemoteHost='192.168.1.99',
+#     # pro tip: never thrust the error output coming from the upnp device,
+#     # it could name a problem that is not the actual problem
+#     NewRemoteHost='',
+#     NewExternalPort=int(5010),
+#     NewProtocol='TCP',
+#     NewInternalPort=int(5010),
+#     NewInternalClient='192.168.1.99',
+#     NewEnabled='true',
+#     NewPortMappingDescription='Testing',
+#     NewLeaseDuration=10000)
+# )
 
 print(d.WANIPConn1.DeletePortMapping(
     # NewRemoteHost='192.168.1.99',
