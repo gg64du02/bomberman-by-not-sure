b93aec3ace8053b32d1138982ab6aa2f0bd1f432
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-08-12 13:13:39 +0200

    Working on the server and ai process

--- bomberman-main.py
+++ bomberman-main.py
@@ -772,11 +772,98 @@ DEFAULT_HOSTING_A_SERVER_PORT = 5008
 
 def AI_proc():
     print("AI_proc:start")# TCP connexion handling
+
+    # server_address = ('localhost', 10000)
+    # s.connect(server_address)
+    # s.send('lol1'.encode())
+    # print(s.recv(1024))
+
+    st_time  = time.time()
+
+    end_of_round_time = time.time()
+
+    # TODO: multithread on port with port listenning
+
+    while(True):
+        print("AI_proc:==========================================================")
+        # select : start
+
+
+        # select : end
+        Controls = keyboardRead()
+
+        ColisionCheckAndMovement()
+
+        if(keyboard.is_pressed('esc')):
+            runningMain = False
+            print("AI_proc:issuing the esc key")
+
+        # gameDisplay.fill(gray)
+        # crate(0,0)
+
+        # displayCrates()
+        # displayMap()
+        # displayBombs()
+        print("AI_proc:brokenCrates",brokenCrates)
+        displayBrokenCratesAndUpdateCollision(False)
+        playersPickupsItems()
+        # displayAirBlasts()
+        # done:Score display is slow
+        if(boolDisplayScores == True):
+            print("AI_proc:displayScores()")
+            displayScores()
+            # debugging/testing purposes
+            # newRound()
+        # done: needs to be debugged
+        # displayPlayers()
+        # displayItems()
+        # if more than 1 players are alive, the round can continue
+        if(numberOfPlayersAlive()>1):
+            end_of_round_time = time.time()
+        if((time.time() - end_of_round_time)*1000>3000):
+            newRound()
+        # for debugging purpose for now
+        # diplayAllAirBlast()
+        # print("airBlastDisplay\n",airBlastDisplay)
+
+        checkForExplodingBomb()
+
+        # print("hitboxes():\n",hitboxes())
+
+        # pygame.display.update()
+        print('AI_proc:time:',str((time.time()-st_time)*1000),'ms')
+        clock.tick(60)
+        st_time = time.time()
+        # print('lol')
+
+
     print("AI_proc:end")
     pass
 
 def server_proc(port):
     print("server_proc:start")
+
+    import select
+    import socket
+    import sys
+    import queue
+
+    # Create a TCP/IP socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setblocking(0)
+    # Bind the socket to the port
+    server_address = ('localhost', 10000)
+    print('starting up on {} port {}'.format(*server_address),
+          file=sys.stderr)
+    server.bind(server_address)
+    # Listen for incoming connections
+    server.listen(5)
+    # Sockets from which we expect to read
+    inputs = [server]
+    # Sockets to which we expect to write
+    outputs = []
+    # Outgoing message queues (socket:Queue)
+    message_queues = {}
 
     st_time  = time.time()
 
@@ -786,6 +873,82 @@ def server_proc(port):
 
     while(True):
         print("server_proc:==========================================================")
+        # select : start
+
+        print('server_proc:waiting for the next event', file=sys.stderr)
+        readable, writable, exceptional = select.select(inputs,
+                                                        outputs,
+                                                        inputs, 0)
+
+        # Handle inputs
+        for s in readable:
+
+            if s is server:
+                # A "readable" socket is ready to accept a connection
+                connection, client_address = s.accept()
+                print('server_proc:  connection from', client_address,
+                      file=sys.stderr)
+                connection.setblocking(0)
+                inputs.append(connection)
+
+                # Give the connection a queue for data
+                # we want to send
+                message_queues[connection] = queue.Queue()
+
+            else:
+                data = s.recv(1024)
+                if data:
+                    # A readable client socket has data
+                    print('server_proc:  received {!r} from {}'.format(
+                        data, s.getpeername()), file=sys.stderr,
+                    )
+                    message_queues[s].put(data)
+                    # Add output channel for response
+                    if s not in outputs:
+                        outputs.append(s)
+                else:
+                    # Interpret empty result as closed connection
+                    print('server_proc:  closing', client_address,
+                          file=sys.stderr)
+                    # Stop listening for input on the connection
+                    if s in outputs:
+                        outputs.remove(s)
+                    inputs.remove(s)
+                    s.close()
+
+                    # Remove message queue
+                    del message_queues[s]
+        # Handle outputs
+        for s in writable:
+            try:
+                next_msg = message_queues[s].get_nowait()
+            except queue.Empty:
+                # No messages waiting so stop checking
+                # for writability.
+                print('server_proc:  ', s.getpeername(), 'queue empty',
+                      file=sys.stderr)
+                outputs.remove(s)
+            else:
+                print('server_proc:  sending {!r} to {}'.format(next_msg,
+                                                    s.getpeername()),
+                      file=sys.stderr)
+                s.send(next_msg)
+        # Handle "exceptional conditions"
+        for s in exceptional:
+            print('server_proc:exception condition on', s.getpeername(),
+                  file=sys.stderr)
+            # Stop listening for input on the connection
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
+
+            # Remove message queue
+            del message_queues[s]
+        # time.sleep(1)
+
+
+        # select : end
         Controls = keyboardRead()
 
         ColisionCheckAndMovement()
@@ -827,7 +990,7 @@ def server_proc(port):
         # print("hitboxes():\n",hitboxes())
 
         # pygame.display.update()
-        print('server_proc:time:',str(time.time()-st_time))
+        print('server_proc:time:',str((time.time()-st_time)*1000),'ms')
         clock.tick(60)
         st_time = time.time()
         # print('lol')
@@ -858,7 +1021,7 @@ if __name__ == '__main__':
 
         if(keyboard.is_pressed('esc')):
             runningMain = False
-            print("issuing the esc key")
+            print("main:issuing the esc key")
 
         gameDisplay.fill(gray)
         # crate(0,0)
@@ -866,13 +1029,13 @@ if __name__ == '__main__':
         displayCrates()
         displayMap()
         displayBombs()
-        print("brokenCrates",brokenCrates)
+        print("main:brokenCrates",brokenCrates)
         displayBrokenCratesAndUpdateCollision(True)
         playersPickupsItems()
         displayAirBlasts()
         # done:Score display is slow
         if(boolDisplayScores == True):
-            print("displayScores()")
+            print("main:displayScores()")
             displayScores()
             # debugging/testing purposes
             # newRound()
@@ -893,7 +1056,7 @@ if __name__ == '__main__':
         # print("hitboxes():\n",hitboxes())
 
         pygame.display.update()
-        print('time:',str(time.time()-st_time))
+        print('main:time:',str(time.time()-st_time))
         clock.tick(60)
         st_time = time.time()
         # print('lol')
