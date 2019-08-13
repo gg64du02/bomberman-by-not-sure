ca9b21b85780c96bf956153d22c193612075119e
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-08-13 14:27:52 +0200

    Working on the new protocol

---
+++ new_multiBN_protocol.py
@@ -0,0 +1,25 @@
+# MultiBN constants
+
+
+
+MBN_TCP_CLIENT_JOIN_REQUIRED = 0
+# Used by the client to require to join a game
+# Used by the server to check if a new client can join (any room left ?)
+MBN_TCP_SERVER_JOIN_REFUSED = 1
+# Used by the server to answer to MBN_TCP_CLIENT_JOIN_REQUIRED to refuse a join (Client drop the connexion)
+MBN_TCP_SERVER_JOIN_ACCEPTED = 2
+# Used by the server to answer to MBN_TCP_CLIENT_JOIN_REQUIRED to accept a join
+
+
+MBN_SESSION
+client: MBN_JOIN_REQUIRED
+server: MBN_JOIN_REFUSED
+server: MBN_JOIN_ACCEPTED
+
+MBN_DATA or SESSION ?
+server:SERVER_SLOTS_MAPPING
+client:DATA_PING_MS
+server:NUMBER_OF_AVAILABLE_PLAYERS_SLOTS
+
+
+
